3db9d9d66d2d827b23989fff4f26167b305f5bf9
William Beuil <william.beuil@gmail.com>
2021-02-25 16:17:51 +0100

    Add related v0.5.0 docs

--- sidebars.js
+++ sidebars.js
@@ -1,21 +1,37 @@
 module.exports = {
-  docs: {
-    Introduction: ["intro", "installation", "limitations"],
-    Usage: [
-      {
-        Commands: [
-          {
-            Scan: ["usage/cmd/scan/iac-source", "usage/cmd/scan/output", "usage/cmd/scan/filtering"],
-            Completion: ["usage/cmd/completion/script"],
-          },
-        ],
-        Flags: ["usage/flags/error-reporting", "usage/flags/version-check"],
-      }
-    ],
-    Providers: [
-      {
-        AWS: ["providers/aws/authentication", "providers/aws/resources"],
-      },
-    ],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'Introduction',
+      items: ["intro", "installation", "limitations"],
+    },
+    {
+      type: 'category',
+      label: 'Usage',
+      items: [
+        {
+          Commands: [
+            {
+              Scan: ["usage/cmd/scan/iac-source", "usage/cmd/scan/output", "usage/cmd/scan/filtering"],
+              Completion: ["usage/cmd/completion/script"],
+            },
+          ],
+        },
+        {
+          Flags: ["usage/flags/error-reporting", "usage/flags/version-check"],
+        },
+        'usage/log-level',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Providers',
+      items: [
+        {
+          AWS: ["providers/aws/authentication", "providers/aws/resources"],
+          GitHub: ["providers/github/authentication", "providers/github/resources"],
+        },
+      ],
+    }
+  ],
 };
