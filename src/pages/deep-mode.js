242a012df9d450040b27bc99ecf3e69b4aba0696
William Beuil <william.beuil@gmail.com>
2021-07-22 16:21:15 +0200

    Add deep-mode redirect

---
+++ src/pages/deep-mode.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Redirect } from "@docusaurus/router";
+import { useLatestVersion } from "@theme/hooks/useDocs";
+
+const DeepMode = () => {
+  const latestVersion = useLatestVersion();
+
+  return <Redirect to={`${latestVersion.path}/usage/cmd/scan-usage#--deep`} />;
+};
+
+export default DeepMode;
