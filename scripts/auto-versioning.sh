5d4d4b699961bf56416f8b17a8fac23869425821
sundowndev <raphael@crvx.fr>
2022-01-14 14:36:01 +0400

    refactor(scripts): auto versioning
    Improve parsing and grammar.

--- scripts/auto-versioning.sh
+++ scripts/auto-versioning.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 VERSION=$(curl -s https://api.github.com/repos/snyk/driftctl/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//')
-MINOR_VERSION=$(echo $VERSION | sed 's/\.[[:digit:]]\+$//g')
+MINOR_VERSION="$(echo "$VERSION" | sed 's/\.[[:digit:]]\+$//g')"
 
 echo "Running script for version $VERSION (minor: $MINOR_VERSION)"
 
 # Check if the version already exist
 if cat versions.json | grep -q $VERSION &>/dev/null; then
-  echo "Skipping new version $VERSION since it already exist"
+  echo "Skipping new version $VERSION since it already exists"
   exit 0
 fi
 
